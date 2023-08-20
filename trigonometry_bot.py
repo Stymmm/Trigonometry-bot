fcc8beaf7dee9e746e1df881f492a38e19b68cc0
Stymmm <142719141+Stymmm@users.noreply.github.com>
2023-08-20 15:31:18 +0530

    Create trigonometry_bot.py
    **Trigonometry Bot: A Python Script for Solving Trigonometric Identities**
    
    Welcome to the Trigonometry Bot repository! This project hosts a Python script (`trigonometry_bot.py`) designed to help you solve various trigonometric identities effortlessly. Whether you're a student studying trigonometry or someone looking for quick solutions to trigonometric equations, this script has got you covered.
    
    **Features:**
    - Quickly solve a variety of trigonometric identities, including Pythagorean, Reciprocal, Quotient, Co-Function, Double Angle, and Half Angle Identities.
    - Get instant answers by inputting the desired identity or equation. The bot will compute and display the results.
    - User-friendly interface with clear instructions and responses.
    
    **How to Use:**
    1. Clone or download the repository to your local machine.
    2. Run the `trigonometry_bot.py` script using a Python interpreter.
    3. Enter the trigonometric identity or equation you'd like to solve.
    4. The bot will process your input, compute the result, and display it on the screen.
    
    **Contributing:**
    If you're passionate about mathematics, coding, or both, we invite you to contribute to this project. You can suggest new trigonometric identities, improve the user interface, or enhance the error handling of the script. To contribute, simply fork this repository, make your changes, and submit a pull request.
    
    **License:**
    This project is open-source and available under the [MIT License](LICENSE). Feel free to use, modify, and distribute the script according to the terms of the license.
    
    **Contact:**
    Have questions, suggestions, or feedback? Feel free to open an issue on this repository or reach out to the project owner via their GitHub profile.
    
    Thank you for checking out the Trigonometry Bot project! We hope this script simplifies your trigonometry problem-solving experience.

---
+++ trigonometry_bot.py
@@ -0,0 +1,72 @@
+import re
+import math
+
+def solve_math_expression(expression):
+    try:
+        result = eval(expression)
+        return result
+    except:
+        return "Invalid expression or operation."
+
+def main():
+    print("Trigonometric Questions Solver Bot")
+    print("Type 'exit' to quit.")
+
+    trig_identities = [
+        # Pythagorean Identities
+        {"question": "sin^2(θ) + cos^2(θ)", "expression": "1", "variables": {"θ": 0}},
+        {"question": "tan^2(θ) + 1", "expression": "sec^2(θ)", "variables": {"θ": 0}},
+        {"question": "cot^2(θ) + 1", "expression": "csc^2(θ)", "variables": {"θ": 0}},
+
+        # Reciprocal Identities
+        {"question": "csc(θ)", "expression": "1 / sin(θ)", "variables": {"θ": 0}},
+        {"question": "sec(θ)", "expression": "1 / cos(θ)", "variables": {"θ": 0}},
+        {"question": "cot(θ)", "expression": "1 / tan(θ)", "variables": {"θ": 0}},
+
+        # Quotient Identities
+        {"question": "tan(θ)", "expression": "sin(θ) / cos(θ)", "variables": {"θ": 0}},
+        {"question": "cot(θ)", "expression": "cos(θ) / sin(θ)", "variables": {"θ": 0}},
+
+        # Co-Function Identities
+        {"question": "sin(π/2 - θ)", "expression": "cos(θ)", "variables": {"θ": 0}},
+        {"question": "cos(π/2 - θ)", "expression": "sin(θ)", "variables": {"θ": 0}},
+        {"question": "tan(π/2 - θ)", "expression": "1 / tan(θ)", "variables": {"θ": 0}},
+        {"question": "cot(π/2 - θ)", "expression": "1 / cot(θ)", "variables": {"θ": 0}},
+
+        # Double Angle Identities
+        {"question": "sin(2θ)", "expression": "2 * sin(θ) * cos(θ)", "variables": {"θ": 0}},
+        {"question": "cos(2θ)", "expression": "cos(θ)**2 - sin(θ)**2", "variables": {"θ": 0}},
+        {"question": "tan(2θ)", "expression": "2 * tan(θ) / (1 - tan(θ)**2)", "variables": {"θ": 0}},
+
+        # Half Angle Identities
+        {"question": "sin(θ/2)", "expression": "sqrt((1 - cos(θ)) / 2)", "variables": {"θ": 0}},
+        {"question": "cos(θ/2)", "expression": "sqrt((1 + cos(θ)) / 2)", "variables": {"θ": 0}},
+        {"question": "tan(θ/2)", "expression": "sqrt((1 - cos(θ)) / (1 + cos(θ)))", "variables": {"θ": 0}},
+
+        # Sum and Difference Identities
+        # ... Add more identities here ...
+
+        # Product-to-Sum Identities
+        # ... Add more identities here ...
+    ]
+
+    while True:
+        user_input = input("Enter a math question: ")
+
+        if user_input.lower() == "exit":
+            print("Exiting the bot.")
+            break
+
+        for identity in trig_identities:
+            if identity["question"] in user_input:
+                expression = identity["expression"]
+                for variable, value in identity["variables"].items():
+                    expression = expression.replace(variable, str(value))
+                result = solve_math_expression(expression)
+                print("Answer:", result)
+                break
+        else:
+            print("Identity not recognized. Please enter a valid trigonometric identity.")
+
+if __name__ == "__main__":
+    main()
